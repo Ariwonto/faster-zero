c9aecc6ad605c49bb05e5751d62b6798519740db
Carlos Tovar <carlos.tovar@ariwonto.com>
2020-02-13 21:55:30 -0500

    faster zero

---
+++ keyStorage.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const { debug, endpoint, secretKey, sensorId } = require('./config.json');
+
+// Push key to data marketplace.
+exports.storeKey = async (sidekey, root, time, live) => {
+  if (debug) return 'Debug mode';
+
+  const packet = {
+    sidekey,
+    root,
+    time,
+    live,
+  };
+
+  try {
+    // Initiate Fetch Call
+    const resp = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: sensorId, packet, sk: secretKey }),
+    });
+    return await resp.json();
+  } catch (error) {
+    console.log('storeKey error', error);
+    return error;
+  }
+};
